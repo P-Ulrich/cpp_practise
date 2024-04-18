95865b38ab2247ac9737d4252f5a12a1cb0e1dbf
Paula Ulrich <paula-ulrich@gmx.de>
2024-04-18 13:19:08 +0200

    new Version

--- Paula/practise01/main.cpp
+++ Paula/practise01/main.cpp
@@ -5,38 +5,38 @@
 
 using namespace std;
 
-double pi_stones(int n) {
+double throw_stones_in_circle(int num_stones) {
     random_device rd;
     mt19937 gen(rd());
     uniform_real_distribution<> dis(0.0, 1.0);
 
-    double random_x_val;
-    double random_y_val;
-    double distance;
-    int circle = 0;
-    int square = 0;
+    auto random_x_val = double{};
+    auto random_y_val = double{};
+    auto distance_origin_squared = double{};
+    auto circle_stone_counts = int{};
+    auto quartered_circle =int{4};
+    auto radius_circle_squared= double{1.};
 
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < num_stones; ++i) {
         random_x_val = dis(gen);
         random_y_val = dis(gen);
-        distance = sqrt(random_x_val * random_x_val + random_y_val * random_y_val);
+         distance_origin_squared= (random_x_val * random_x_val + random_y_val * random_y_val);
 
-        if (distance <= 1)
-            circle++;
+        if (distance_origin_squared <= radius_circle_squared)
+            ++circle_stone_counts;
 
-        square++;
     }
 
-    return double(4 * circle) / square;
+    return static_cast<double>(quartered_circle * circle_stone_counts) / num_stones;
 }
 
-double pi_std_err_2(int n, int precision) {
+double pi_std_err_2(int num_stones, int precision) {
     vector<double> pi_estimates(precision);
-    double sum_pi = 0;
-    double sum_pi_squared = 0;
+    auto sum_pi = double{};
+    auto sum_pi_squared = double{};
 
     for (int i = 0; i < precision; ++i) {
-        pi_estimates[i] = pi_stones(n);
+        pi_estimates[i] = throw_stones_in_circle(num_stones);
         sum_pi += pi_estimates[i];
     }
 
@@ -46,19 +46,18 @@ double pi_std_err_2(int n, int precision) {
         sum_pi_squared += (pi_estimates[i] - mean_pi) * (pi_estimates[i] - mean_pi);
     }
 
-    double variance = sum_pi_squared / precision;
-    double std_dev = sqrt(variance);
-    double std_err = std_dev / sqrt(precision);
+    auto variance = double{sum_pi_squared / precision};
+    auto std_dev = double{sqrt(variance)};
 
-    return std_err;
+    return std_dev;
 }
 
 int main() {
-    int n = 10000;   // Anzahl der Punkte
-    int pre = 1000;  // Anzahl der Schätzungen
-    double result = pi_stones(n);
-    double err = pi_std_err_2(n, pre);
+    auto num_stones = 10000;   // Anzahl der Punkte
+    auto precision = 1000;  // Anzahl der Schätzungen
+    auto result = throw_stones_in_circle(num_stones);
+    auto err = pi_std_err_2(num_stones, precision);
     cout << "Approximated value of PI: " << result << endl;
-    cout << "Approximated error of PI: " << err << endl;
+    cout << "Approximated deviation of PI: " << err << endl;
     return 0;
 }
