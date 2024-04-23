68a903548fa7add6f80a46c67502b9f8ec296aa8
Paula Ulrich <paula-ulrich@gmx.de>
2024-04-23 10:43:46 +0200

    class

--- Paula/practise01/main.cpp
+++ Paula/practise01/main.cpp
@@ -1,11 +1,100 @@
+#include <cmath>
 #include <iostream>
 #include <random>
-#include <cmath>
 #include <vector>
 
 using namespace std;
 
-double throw_stones_in_circle(int num_stones) {
+// struct Result {
+//   double mean;
+//   double err_pi;
+// };
+//
+// double calculate_pi(int num_stones) {
+//   random_device rd;
+//   mt19937 gen(rd());
+//   uniform_real_distribution<> dis(0.0, 1.0);
+//
+//   auto random_x_val = double{};
+//   auto random_y_val = double{};
+//   auto distance_origin_squared = double{};
+//   auto circle_stone_counts = int{};
+//   auto quartered_circle = int{4};
+//   auto radius_circle_squared = double{1.};
+//
+//   for (int i = 0; i < num_stones; ++i) {
+//     random_x_val = dis(gen);
+//     random_y_val = dis(gen);
+//     distance_origin_squared =
+//         (random_x_val * random_x_val + random_y_val * random_y_val);
+//
+//     if (distance_origin_squared <= radius_circle_squared)
+//       ++circle_stone_counts;
+//   }
+//
+//   return static_cast<double>(quartered_circle * circle_stone_counts) /
+//          num_stones;
+// }
+//
+// auto pi_mean_and_err(int num_stones, int precision) {
+//
+//   auto pi_result = Result{};
+//
+//   vector<double> pi_estimates(precision);
+//   auto sum_pi = double{};
+//   auto sum_pi_squared = double{};
+//
+//   for (int i = 0; i < precision; ++i) {
+//     pi_estimates[i] = calculate_pi(num_stones);
+//     sum_pi += pi_estimates[i];
+//   }
+//
+//   double mean_pi = sum_pi / precision;
+//
+//   pi_result.mean = mean_pi;
+//
+//   for (int i = 0; i < precision; ++i) {
+//     sum_pi_squared += (pi_estimates[i] - mean_pi) * (pi_estimates[i] -
+//     mean_pi);
+//   }
+//
+//   auto variance = sum_pi_squared / precision;
+//
+//   pi_result.err_pi = variance;
+//   return pi_result;
+// }
+//
+// int main() {
+//   // auto num_stones = 10000;
+//   // auto num_loops = 1000;
+//   // auto pi_results = pi_mean_and_err(num_stones, num_loops);
+//   // cout << "Approximated value of PI: " << pi_results.mean << endl;
+//   // cout << "Approximated deviation of PI: " << pi_results.err_pi << endl;
+//
+//   auto pi_generator = PiGenerator{};
+//   pi_generator.set_num_of_loops(10000);
+//
+//   auto results = pi_generator.get_results();
+//   std::cout << "Value: " << results.value << "\terror:" << results.error
+//             << "\n";
+//   return 0;
+// }
+
+struct Result {
+  double value;
+  double error;
+};
+class PiGenerator {
+private:
+  int num_stones;
+  int num_loops;
+  Result pi_result;
+
+public:
+  void set_num_of_loops(int loops) { num_loops = loops; }
+  void set_num_of_stones(int stones) { num_stones = stones; }
+
+  double calculate_pi(int num_stones) {
     random_device rd;
     mt19937 gen(rd());
     uniform_real_distribution<> dis(0.0, 1.0);
@@ -14,49 +103,60 @@ double throw_stones_in_circle(int num_stones) {
     auto random_y_val = double{};
     auto distance_origin_squared = double{};
     auto circle_stone_counts = int{};
-    auto quartered_circle =int{4};
-    auto radius_circle_squared= double{1.};
+    auto quartered_circle = int{4};
+    auto radius_circle_squared = double{1.};
 
     for (int i = 0; i < num_stones; ++i) {
-        random_x_val = dis(gen);
-        random_y_val = dis(gen);
-         distance_origin_squared= (random_x_val * random_x_val + random_y_val * random_y_val);
+      random_x_val = dis(gen);
+      random_y_val = dis(gen);
+      distance_origin_squared =
+          (random_x_val * random_x_val + random_y_val * random_y_val);
 
-        if (distance_origin_squared <= radius_circle_squared)
-            ++circle_stone_counts;
-
+      if (distance_origin_squared <= radius_circle_squared)
+        ++circle_stone_counts;
     }
 
-    return static_cast<double>(quartered_circle * circle_stone_counts) / num_stones;
-}
+    return static_cast<double>(quartered_circle * circle_stone_counts) /
+           num_stones;
+  }
 
-double pi_std_err_2(int num_stones, int precision) {
+  void pi_mean_and_err(int num_stones, int precision) {
+
     vector<double> pi_estimates(precision);
     auto sum_pi = double{};
     auto sum_pi_squared = double{};
 
     for (int i = 0; i < precision; ++i) {
-        pi_estimates[i] = throw_stones_in_circle(num_stones);
-        sum_pi += pi_estimates[i];
+      pi_estimates[i] = calculate_pi(num_stones);
+      sum_pi += pi_estimates[i];
     }
 
     double mean_pi = sum_pi / precision;
 
+    pi_result.value = mean_pi;
+
     for (int i = 0; i < precision; ++i) {
-        sum_pi_squared += (pi_estimates[i] - mean_pi) * (pi_estimates[i] - mean_pi);
+      sum_pi_squared +=
+          (pi_estimates[i] - mean_pi) * (pi_estimates[i] - mean_pi);
     }
 
     auto variance = sum_pi_squared / precision;
 
-    return sqrt(variance);
-}
+    pi_result.error = variance;
+  }
 
+  auto get_results() { 
+ pi_mean_and_err(num_stones,num_loops); 
+      return pi_result; }
+};
 int main() {
-    auto num_stones = 10000;   // Anzahl der Punkte
-    auto num_loops = 1000;  
-    auto result = throw_stones_in_circle(num_stones);
-    auto err = pi_std_err_2(num_stones, num_loops);
-    cout << "Approximated value of PI: " << result << endl;
-    cout << "Approximated deviation of PI: " << err << endl;
-    return 0;
+
+  auto pi_generator = PiGenerator{};
+  pi_generator.set_num_of_loops(10000);
+  pi_generator.set_num_of_stones(10000);
+
+  auto results = pi_generator.get_results();
+  std::cout << "Value: " << results.value << "\terror:" << results.error
+            << "\n";
+  return 0;
 }
